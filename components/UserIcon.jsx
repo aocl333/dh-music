9699b79f12f172d06eadabc2fba507a358a2f60d
김해진 <hjk@gimhaejin-ui-Macmini.local>
2024-04-17 13:28:01 +0900

    update

---
+++ components/UserIcon.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+
+const UserIcon = ({ size = "sm" }) => {
+  return (
+    <Avatar
+      className={cn(
+        "w-[26px] h-[26px]" && size === "lg" && "w-[56px] h-[56px] "
+      )}
+    >
+      <AvatarImage src="https://avatars.githubusercontent.com/u/56382506?v=4" />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+  );
+};
+
+export default UserIcon;
